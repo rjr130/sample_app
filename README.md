83164e277c86f3b623e0e6bfab40088b08236c7b
Junrui Ruan <rjr130@gmail.com>
2014-04-24 16:30:44 -0500

    Rebuild the Repo and Update all the files

---
+++ README.md
@@ -0,0 +1,4 @@
+# UW-Madison CS 369 Course Homework Project 
+
+This is the homework application for CS 369 @UW-Madison
+by [Junrui Ruan](http://junruiruan.com/).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '37adddc08de08a157ad6203ee0b8a7e58bef1153806b9701d0915d524ef2a6e9d70bc1935cea0d45f0e9d2d74b091e9261b343a64cc5df965c3ffc5ee49e1369'
